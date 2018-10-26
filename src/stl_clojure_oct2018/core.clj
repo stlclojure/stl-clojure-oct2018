6c99a441d92f5a051058225c369948ecd462ebd6
Christopher Mark Gore <cgore@cgore.com>
2018-10-25 19:24:57 -0500

    sample from the book

--- src/stl_clojure_oct2018/core.clj
+++ src/stl_clojure_oct2018/core.clj
@@ -1,6 +1,22 @@
 (ns stl-clojure-oct2018.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; https://www.braveclojure.com/multimethods-records-protocols/
+
+(defmulti full-moon-behavior
+  (fn [were-creature] (:were-type were-creature)))
+
+(defmethod full-moon-behavior :wolf
+  [were-creature]
+  (str (:name were-creature) " will howl and murder"))
+
+(defmethod full-moon-behavior :simmons
+  [were-creature]
+  (str (:name were-creature) " will encourage people and sweat to the oldies"))
+
+(full-moon-behavior {:were-type :wolf
+                     :name "Rachel from next door"})
+;; => "Rachel from next door will howl and murder"
+
+(full-moon-behavior {:name "Andy the baker"
+                     :were-type :simmons})
+;; => "Andy the baker will encourage people and sweat to the oldies"
